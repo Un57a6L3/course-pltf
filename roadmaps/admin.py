380949480ddbec519a290aa0fa1758a51d610c88
Arseny Antonov <un57a6l3@gmail.com>
2024-06-13 17:11:30 +0300

    Implemented user profile model

--- roadmaps/admin.py
+++ roadmaps/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Trajectory, TrajElement, Course
+from .models import Trajectory, TrajElement, Course, Profile
 
 admin.site.register(Trajectory)
 admin.site.register(TrajElement)
 admin.site.register(Course)
+admin.site.register(Profile)
--- roadmaps/models.py
+++ roadmaps/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
 
 
 class Course(models.Model):
@@ -47,3 +50,20 @@ class TrajElement(models.Model):
     def get_tags(self):
         return self.tags.split(',')
     
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    role = models.CharField(max_length=50, default='student')
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(pre_save, sender=User)
+def create_user_profile(sender, instance, **kwargs):
+    if not hasattr(instance, 'profile'):
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
